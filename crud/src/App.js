92dd0960195a20048611c33dd84b6408b067124f
mutinhiri <barnabmutinhiri@gmail.com>
2022-01-14 21:41:57 +0200

    Add react container and stack

--- crud/src/App.js
+++ crud/src/App.js
@@ -1,8 +1,15 @@
-import './App.css';
+import Container from 'react-bootstrap/Container'
+import { Stack } from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
+     <Container>
+        <Stack direction='horizontal' gap="2" className="mb-4" >
+
+        </Stack>
+
+      </Container>
      
     </div>
   );

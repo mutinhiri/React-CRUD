f72409bcb9440af86651657fdab34869be9266ae
mutinhiri <barnabmutinhiri@gmail.com>
2022-01-14 21:48:35 +0200

    Add buttons from react

--- crud/src/App.js
+++ crud/src/App.js
@@ -1,14 +1,15 @@
 import Container from 'react-bootstrap/Container'
-import { Stack } from 'react-bootstrap'
+import { Button, Stack } from 'react-bootstrap'
 
 function App() {
   return (
     <div className="App">
      <Container>
         <Stack direction='horizontal' gap="2" className="mb-4" >
-
+          <h1 className='me-auto'>Budgets</h1>
+          <Button variant='primary'>Add budget</Button>
+          <Button variant='outline-primary'>Add expense</Button>
         </Stack>
-
       </Container>
      
     </div>

d51d6b8f2fcac8120aa6248378c0b12bc1529d55
mutinhiri <barnabmutinhiri@gmail.com>
2022-01-14 22:12:31 +0200

    currency formater initialise

---
+++ crud/src/components/Budgetcard.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Card } from 'react-bootstrap'
+import { currencyFormatter } from '../utils'
+
+export default function Budgetcard({name, amount, max}) {
+  return (
+    <div>
+      <Card>
+        <Card.Body>
+          <Card.Title>
+            <div>{name}</div>
+            <div>{currencyFormatter.format (amount)} / { max}</div>
+          </Card.Title>
+        </Card.Body>
+      </Card>
+    </div>
+  )
+}

44ed3ed88d768f925df01edcd3460b1c6f126109
mutinhiri <barnabmutinhiri@gmail.com>
2022-01-14 22:31:46 +0200

    Add component styling

--- crud/src/components/Budgetcard.js
+++ crud/src/components/Budgetcard.js
@@ -7,8 +7,8 @@ export default function Budgetcard({name, amount, max}) {
     <div>
       <Card>
         <Card.Body>
-          <Card.Title>
-            <div>{name}</div>
+          <Card.Title className='d-flex justify-content-between align-items-baseline fw-normal mb-3'>
+            <div className='me-2'>{name}</div>
             <div>{currencyFormatter.format (amount)} / {currencyFormatter.format (max)}</div>
           </Card.Title>
         </Card.Body>

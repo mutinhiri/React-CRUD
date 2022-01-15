958bc0a38fbfc448a79941f19c7b08d056a2be80
mutinhiri <barnabmutinhiri@gmail.com>
2022-01-15 17:39:27 +0200

    set button toggle for App

--- crud/src/App.js
+++ crud/src/App.js
@@ -2,14 +2,16 @@ import Container from 'react-bootstrap/Container'
 import { Button, Stack } from 'react-bootstrap'
 import Budgetcard from './components/Budgetcard';
 import AddBudgetModal from './components/AddBudgetModal';
+import { useState } from 'react';
 
 function App() {
+  const [showAddBudgetModal, setShowAddBudgetModal ] = useState(false)
   return (
     <div className="App">
      <Container className='my-4'>
         <Stack direction='horizontal' gap="3" className="mb-4" >
           <h1 className='me-auto'>Budgets</h1>
-          <Button variant='primary'>Add budget</Button>
+          <Button variant='primary' onClick={() =>setShowAddBudgetModal(true)}>Add budget</Button>
           <Button variant='outline-primary'>Add expense</Button>
         </Stack>
         <div className='stacks'>

e3e3ada15f4e931fdd23caed5aade58677e90703
mutinhiri <barnabmutinhiri@gmail.com>
2022-01-15 17:11:51 +0200

    Add handleSubmit function

--- crud/src/components/AddBudgetModal.js
+++ crud/src/components/AddBudgetModal.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import { useRef } from 'react'
 import { Button, Form, Modal } from 'react-bootstrap'
+import { useBudgets } from './contexts/BudgetsContext'
 
 export default function AddBudgetModal({ show, handleClose }) {
   const nameRef = useRef()
   const maxRef = useRef()
-  
-  function hanldeSubmit(e){}
+  const {addBudget} = useBudgets()
+
+  function hanldeSubmit(e) {
+    e.preventDefault()
+    addBudget(
+    {
+      name: nameRef.current.value,
+      max: parseFloat(maxRef.current.value)
+    })
+   
+  }
   return (
     <Modal show={show} onHide={handleClose}>
       <Form onSubmit ={hanldeSubmit}>
@@ -22,7 +32,7 @@ export default function AddBudgetModal({ show, handleClose }) {
           </Form.Group>
           <Form.Group controlId='max' className='mb-3'>
             <Form.Label>Maximum Spending</Form.Label>
-            <Form.Control type='number' required min={0} step={0.01} ></Form.Control>
+            <Form.Control ref={maxRef} type='number' required min={0} step={0.01} ></Form.Control>
           </Form.Group>
           <div className="d-flex justify-content-end">
             <Button variant='primary' type='submit'>Add</Button>

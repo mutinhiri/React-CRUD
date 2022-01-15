1375410e5c769639ba4bdeba1f246ee456c709f6
mutinhiri <barnabmutinhiri@gmail.com>
2022-01-15 16:50:56 +0200

    Add form component

--- crud/src/components/AddBudgetModal.js
+++ crud/src/components/AddBudgetModal.js
@@ -1,10 +1,21 @@
 import React from 'react'
-import { Modal } from 'react-bootstrap'
+import { Form, Modal } from 'react-bootstrap'
 
-export default function AddBudgetModal() {
+export default function AddBudgetModal({show, handleClose}) {
   return (
     <Modal show={show} onHide={handleClose}>
-
+      <Form onSubmit ={hanldeSubmit}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            New Budget
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form.Group>
+            <Form.Label>Name</Form.Label>
+          </Form.Group>
+        </Modal.Body>
+      </Form>
     </Modal>
   )
 }

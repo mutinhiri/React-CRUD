51addc80d0401b33c281b2bd1a6bcae9611578d0
mutinhiri <barnabmutinhiri@gmail.com>
2022-01-15 12:55:24 +0200

    Add stacks and buttons to add expenses

--- crud/src/App.js
+++ crud/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
      <Container className='my-4'>
-        <Stack direction='horizontal' gap="2" className="mb-4" >
+        <Stack direction='horizontal' gap="3" className="mb-4" >
           <h1 className='me-auto'>Budgets</h1>
           <Button variant='primary'>Add budget</Button>
           <Button variant='outline-primary'>Add expense</Button>
--- crud/src/components/Budgetcard.js
+++ crud/src/components/Budgetcard.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Card } from 'react-bootstrap'
+import { Button, Card, ProgressBar, Stack } from 'react-bootstrap'
 import { currencyFormatter } from '../utils'
 
 export default function Budgetcard({name, amount, max}) {
@@ -9,10 +9,32 @@ export default function Budgetcard({name, amount, max}) {
         <Card.Body>
           <Card.Title className='d-flex justify-content-between align-items-baseline fw-normal mb-3'>
             <div className='me-2'>{name}</div>
-            <div>{currencyFormatter.format (amount)} / {currencyFormatter.format (max)}</div>
+            <div className='d-flex align-items-baseline'> 
+              {currencyFormatter.format(amount)}
+              <span className='text-muted fs-6'>
+                / {currencyFormatter.format(max)}
+              </span>
+            </div>
           </Card.Title>
+          <ProgressBar className='rounded-pill' variant={getProgressBarVariant(amount, max)}
+            min={0}
+            max={max}
+            now={amount}
+          />
+          <Stack direction='horizontal' gap='2' className="mt-4" >
+            <Button variant='outline-primary' className='ms-auto'>Add Expense</Button>
+            <Button variant='outline-secondary'>View Expense</Button>
+
+          </Stack>
         </Card.Body>
       </Card>
     </div>
   )
+}
+
+function getProgressBarVariant(amount, max) {
+  const ratio = amount / max
+  if (ratio < .5) return "primary"
+  if (ratio < .75) return "warning"
+  return "danger"
 }

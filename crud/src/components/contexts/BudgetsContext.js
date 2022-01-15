eb918421b389090fb3a8e75b4fa79eaa1a654496
mutinhiri <barnabmutinhiri@gmail.com>
2022-01-15 13:56:42 +0200

    Add useContext hooks

--- crud/src/components/contexts/BudgetsContext.js
+++ crud/src/components/contexts/BudgetsContext.js
@@ -1,7 +1,44 @@
+import React, { useContext } from "react"
+
+const BudgetsContext = React.createContext
+
 export function useBudgets() {
+  return useContext(BudgetsContext)
   
 }
 
 export const BudgetsProvider = ({ children }) => {
-  return children
+  const budgets = useState([])
+  const expenses = []
+
+  function getBudgetExpenses() {
+    
+  }
+
+  function addExpenses() {
+    
+  }
+  function addBudget() {
+
+  }
+
+  function deleteBudget() {
+    
+  }
+
+  function deleteExpense() {
+    
+  }
+
+  return (
+    <BudgetsContext.Provider value={{
+      budgets,
+      expenses,
+      getBudgetExpenses,
+      addExpenses,
+      addBudget,
+      deleteBudget,
+      deleteExpense
+    }}>{children}</BudgetsContext.Provider>
+  )
 }
